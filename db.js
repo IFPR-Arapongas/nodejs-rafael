2870349a38af4be4c037d2e121aaeea213692028
rafael16rc-git <rafael16rc@gmail.com>
2025-12-03 21:53:43 -0300

    a

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
+const mysql = require("mysql2/promise");
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "cadastro",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+module.exports = pool;
 
 export async function getClientes() {
   const [rows] = await pool.query('SELECT * FROM cliente');
